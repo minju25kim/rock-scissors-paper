c96caaa672a3b7ed05fac1957eeae5a4ef1d4066
Minju Kim <kmj673@gmail.com>
2021-11-15 17:31:51 +0000

    Add steps as pseudo code to solve the problem in js file

---
+++ script.js
@@ -0,0 +1,13 @@
+//grab the buttons from html
+button = document.querySelector(".move-button");
+console.log(button.innerHTML);
+
+//get the computer-choice and outcome elements from html
+
+// user chooses a move:
+
+//generate a move for the computer
+
+//work out the winner
+
+//display the winner
