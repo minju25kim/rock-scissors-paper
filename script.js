7307f9e8f04985c4235bebcd07cfea83e0f5fc51
Minju Kim <kmj673@gmail.com>
2021-11-15 18:09:13 +0000

    get user choice

--- script.js
+++ script.js
@@ -8,8 +8,25 @@ const outcomeText = document.querySelector(".outcome-text");
 
 // user chooses a move:
 
+userButtons.forEach((element) => {
+  element.addEventListener("click", userMove);
+});
+
+//assign the user's choice to a variable
+
+const userChoice = (event) => {
+  //identify the target
+  const userChoice = event.target.textContent;
+  return `user chose: ${userChoice}`;
+};
+
 //generate a move for the computer
 
 //work out the winner
 
 //display the winner
+
+function userMove(event) {
+  //   console.log(event);
+  outcomeText.textContent = userChoice(event);
+}
